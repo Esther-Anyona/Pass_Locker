5ae53adb75e8abcb754d38ec07c81cd3a17c0e4a
Esther-Anyona <jkemuntoe@gmail.com>
2022-01-24 22:24:39 +0300

    Create methods for managing credentials and their test cases to ensure they are working correctly

--- Credentials.py
+++ Credentials.py
@@ -2,9 +2,32 @@ class Credentials:
     """
     Credentials class to generate instances of account credentials
     """
-    def __init__(self,accountname, username, passcode):#properties for each credentials object
-        self.accountname = accountname
+    def __init__(self,account, username, passcode):#properties for each credentials object
+        self.account = account
         self.username = username
         self.passcode = passcode
 
     credentialsList =[] #Empty list for object credentials
+
+    def save_account(self):
+        Credentials.credentialsList.append(self)
+
+    def delete_account(self):
+        Credentials.credentialsList.remove(self)
+
+    # @classmethod
+    # def find_by_username(cls, username):
+    #     for Credentials in cls.credentialsList:
+    #         if Credentials.username == username:
+    #             return username
+
+    @classmethod
+    def display_account(cls):
+        return cls.credentialsList
+
+    @classmethod
+    def account_exists(cls, account):
+        for Credentials in cls.credentialsList:
+            if Credentials.account == account:
+                return account
+        return False 
--- credentials_test.py
+++ credentials_test.py
@@ -1,5 +1,6 @@
 import unittest
 from Credentials import Credentials
+import string
 
 
 class TestCredentials(unittest.TestCase):
@@ -13,6 +14,45 @@ class TestCredentials(unittest.TestCase):
         self.new_credentials = Credentials("twitter", "kays", "K321")
 
     def test_init(self):
-        self.assertEqual(self.new_credentials.accountname, "twitter")
+        self.assertEqual(self.new_credentials.account, "twitter")
         self.assertEqual(self.new_credentials.username, "kays")    
-        self.assertEqual(self.new_credentials.passcode, "K321")    
+        self.assertEqual(self.new_credentials.passcode, "K321") 
+
+    def test_save_account(self):
+        self.new_credentials.save_account()
+        self.assertEqual(len(Credentials.credentialsList), 1)   
+
+    def test_save_multiple_accounts(self):
+        self.new_credentials.save_account()
+        test_account = Credentials("facebook", "fbkay", "fb321")
+        test_account.save_account()
+        self.assertEqual(len(Credentials.credentialsList),2)
+
+    def test_delete_account(self):
+        self.new_credentials.save_account()
+        test_account = Credentials("facebook", "fbkay", "fb321")
+        test_account.save_account()
+        self.new_credentials.delete_account()
+        self.assertEqual(len(Credentials.credentialsList),1)
+
+    # def test_find_account_by_name(self):
+    #     self.new_credentials.save_account()
+    #     test_account = Credentials("facebook", "fbkay", "fb321")
+    #     test_account.save_account()
+    #     found_account = Credentials.find_by_name('fbkay')
+    #     self.assertEqual(found_account.username, test_account.username)
+
+    def test_display_account(self):
+        self.assertEqual(Credentials.display_account(), Credentials.credentialsList)
+
+    def test_account_exists(self):
+        self.new_credentials.save_account()
+        test_account = Credentials("facebook", "fbkay", "fb321")
+        test_account.save_account()
+        account_exists = Credentials.account_exists('twitter')
+        self.assertTrue(account_exists)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
